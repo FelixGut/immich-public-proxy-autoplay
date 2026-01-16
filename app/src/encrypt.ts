996f6e1ed1cc72cc1f2323a231cc5a9078386329
FelixGut <104640048+FelixGut@users.noreply.github.com>
2026-01-16 19:23:08 +0100

    added encryption back, but log unecnrypted password

--- app/src/encrypt.ts
+++ app/src/encrypt.ts
@@ -13,10 +13,15 @@ const algorithm = 'aes-256-cbc'
  * Encrypt text data for storing in the session cookie
  */
 export function encrypt (text: string): EncryptedPayload {
+  console.log('Password before encryption:', text)
   try {
+    const ivBuf = crypto.randomBytes(16)
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), ivBuf)
+    let encrypted = cipher.update(text, 'utf8', 'hex')
+    encrypted += cipher.final('hex')
     return {
-      iv: '',
-      cr: text
+      iv: ivBuf.toString('hex'),
+      cr: encrypted
     }
   } catch (e) { }
 
