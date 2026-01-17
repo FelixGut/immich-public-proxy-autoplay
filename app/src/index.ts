24da697b623e9c6ea290ed076e5e76ffb88e77a6
FelixGut <104640048+FelixGut@users.noreply.github.com>
2026-01-17 12:03:57 +0100

    Update index.ts

--- app/src/index.ts
+++ app/src/index.ts
@@ -47,7 +47,7 @@ const decodeCookie = (req: Request, _res: Response, next: NextFunction) => {
   const session = req.session?.[shareKey]
   log('shareKey & session ' + shareKey + ' | ' + session?.iv + ' ' + session?.cr)
   log('req.params.shareType ' + req.params.shareType)
-  log('hard coded shareKey = test ' + req.session?.[test])
+  log('hard coded shareKey = test ' + req.session?.['test'])
   if (shareKey && session?.iv && session?.cr) {
     try {
       const payload = JSON.parse(decrypt({
